f5ea67d4ba947e4375ba85e61e9aedf1bdf3657c
Xobotok <meshcheryakov.m.i@yandex.ru>
2018-09-30 13:14:10 +0500

    + class Product extends Model

--- models/Product.php
+++ models/Product.php
@@ -4,12 +4,15 @@
 namespace app\models;
 
 
-class Product {
+use yii\base\Model;
+
+class Product extends Model {
 public $id;
 public $name;
 public $category;
 public $price;
 function __construct($id, $name, $category, $price) {
+    parent::__construct();
     $this->id = $id;
     $this->name = $name;
     $this->category = $category;
